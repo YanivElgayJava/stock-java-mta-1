dece3b69adda86b4b30bf21e9dd8eadf35796663
yaniv elgay <yanivelgay@gmail.com>
2014-11-17 12:20:26 +0200

    exercise 3 - 1.area of a circle

--- src/com/mta/javacourse/YanivElgay066546110Servlet.java
+++ src/com/mta/javacourse/YanivElgay066546110Servlet.java
@@ -6,14 +6,26 @@ import javax.servlet.http.*;
 public class YanivElgay066546110Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/html");
-		int num1;
-		int num2;
-		int num3;
-		num1 = 4;
-		num2 = 3;
-		num3 = 7;
-		int result = (num1 + num2) * num3;
-		String resultStr = new String("<h1>Result of ("+num1+" + "+num2+") * "+num3+" = "+result+"</h1>");
+		
+		int radius = 50;
+		double area = (Math.pow(radius,2)) * (Math.PI);
+		String circleArea = new String("Area of circle with radius "+radius+" is : "+area+" square-cm");
+		
+		int hypotenuse = 50;
+		int angle = 30;
+		double opposite = (Math.toRadians(angle)) * (hypotenuse);
+		String calcOpposite = new String("Length of opposite where angle B is "+angle+" degrees and hypotenuse length is "+hypotenuse+" cm is :"+opposite+" cm ");
+		
+		int base = 20;
+		int exp = 13;
+		double power = Math.pow(base,exp);
+		String calcPower = new String("Power of "+base+" with exp of "+exp+" is "+power+" ");
+		
+		String line1 = new String(circleArea);
+		String line2 = new String(calcOpposite);
+		String line3 = new String(calcPower);
+	
+		String resultStr = line1 + "<br>" + line2 + "<br>" + line3;
 		resp.getWriter().println(resultStr);
 	}
 }
